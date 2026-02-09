6be49a26f2e3d27063f796db6555b5b141b32cd8
Jason Wen <haibin.wen3@gmail.com>
2026-02-09 02:55:06 -0500

    Reapply "[TIZI/TICI] ui: drive stat cache is a json already"
    This reverts commit 043eab56204ac3b915fbaa24fbc16a7d2420b9d4.

--- selfdrive/ui/sunnypilot/layouts/settings/trips.py
+++ selfdrive/ui/sunnypilot/layouts/settings/trips.py
@@ -4,7 +4,6 @@ Copyright (c) 2021-, Haibin Wen, sunnypilot, and a number of other contributors.
 This file is part of sunnypilot and is licensed under the MIT License.
 See the LICENSE.md file in the root directory for more details.
 """
-import json
 import requests
 import threading
 import time
@@ -54,7 +53,7 @@ class TripsLayout(Widget):
     if not stats:
       return {}
     try:
-      return json.loads(stats)
+      return stats
     except Exception:
       cloudlog.exception(f"Failed to decode drive stats: {stats}")
       return {}
