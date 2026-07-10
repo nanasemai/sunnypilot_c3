da3432637952973206fe7fc65d6f28bfcbdce6e6
nanasemai <42890263+nanasemai@users.noreply.github.com>
2026-07-10 09:56:14 +0800

    docs(ui): 添加并使用多语言翻译“请启动车辆以使用”
    新增了"turn on ignition to use"的简体中文翻译，并在onroad布局中替换硬编码文本为多语言函数tr()

--- selfdrive/ui/body/layouts/onroad.py
+++ selfdrive/ui/body/layouts/onroad.py
@@ -2,6 +2,7 @@ import time
 import pyray as rl
 
 from openpilot.system.ui.lib.application import gui_app, FontWeight
+from openpilot.system.ui.lib.multilang import tr
 from openpilot.system.ui.widgets import Widget
 from openpilot.system.ui.widgets.label import UnifiedLabel
 from openpilot.selfdrive.ui.ui_state import ui_state
@@ -25,7 +26,7 @@ class BodyLayout(Widget):
     self._turning_right = False
     self._last_input_time = time.monotonic()
     self._was_active = False
-    self._offroad_label = UnifiedLabel("turn on ignition to use", 95 if gui_app.big_ui() else 45, FontWeight.DISPLAY,
+    self._offroad_label = UnifiedLabel(tr("turn on ignition to use"), 95 if gui_app.big_ui() else 45, FontWeight.DISPLAY,
                                        alignment=rl.GuiTextAlignment.TEXT_ALIGN_CENTER,
                                        alignment_vertical=rl.GuiTextAlignmentVertical.TEXT_ALIGN_MIDDLE)
 
